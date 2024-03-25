416c74326d76594b603919ad52d4552257c518f0
quicksc0p3r <80166934+quicksc0p3r@users.noreply.github.com>
2024-03-25 21:55:53 +0500

    Remove unused imports

--- app/src/main/java/org/quicksc0p3r/discordtimestamp/KeyboardScreen.kt
+++ app/src/main/java/org/quicksc0p3r/discordtimestamp/KeyboardScreen.kt
@@ -1,22 +1,10 @@
 package org.quicksc0p3r.discordtimestamp
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.Layout
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 @Composable
