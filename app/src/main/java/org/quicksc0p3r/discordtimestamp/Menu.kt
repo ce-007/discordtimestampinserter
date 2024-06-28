f58a4ad798c466e6ca4bcd8d07a1cbc77019056f
quicksc0p3r <80166934+quicksc0p3r@users.noreply.github.com>
2024-06-28 21:46:30 +0500

    Add button to open IME picker
    Closes #3

--- app/src/main/java/org/quicksc0p3r/discordtimestamp/Menu.kt
+++ app/src/main/java/org/quicksc0p3r/discordtimestamp/Menu.kt
@@ -1,20 +1,40 @@
 package org.quicksc0p3r.discordtimestamp
 
+import android.content.Context.INPUT_METHOD_SERVICE
+import android.view.inputmethod.InputMethodManager
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Keyboard
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.PlainTooltip
 import androidx.compose.material3.Text
+import androidx.compose.material3.TooltipBox
+import androidx.compose.material3.TooltipDefaults
+import androidx.compose.material3.rememberTooltipState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Menu(changeCurrentScreen: (CurrentScreen) -> Unit, changeTimestampType: (TimestampType) -> Unit) {
+    val context = LocalContext.current
+    val inputMethodManager = context.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+
     Column(
         modifier = Modifier
             .background(MaterialTheme.colorScheme.background)
@@ -75,13 +95,24 @@ fun Menu(changeCurrentScreen: (CurrentScreen) -> Unit, changeTimestampType: (Tim
         }
         Row(
             modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceEvenly
+            horizontalArrangement = Arrangement.SpaceBetween
         ) {
+            IconButton(onClick = {}, enabled = false) {} // Workaround for proper centering
             FilledTonalButton(onClick = {
                 changeCurrentScreen(CurrentScreen.TIME_PICKER_NEXT_DATE)
                 changeTimestampType(TimestampType.RELATIVE)
             } ) {
                 Text("Relative")
+            }
+            TooltipBox(
+                modifier = Modifier.padding(end = 5.dp),
+                positionProvider = TooltipDefaults.rememberPlainTooltipPositionProvider(),
+                tooltip = { PlainTooltip { Text(stringResource(R.string.choose_input_method)) } },
+                state = rememberTooltipState()
+            ) {
+                IconButton(onClick = { inputMethodManager.showInputMethodPicker() }) {
+                    Icon(imageVector = Icons.Rounded.Keyboard, contentDescription = stringResource(R.string.choose_input_method))
+                }
             }
         }
     }
